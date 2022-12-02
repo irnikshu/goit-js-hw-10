983c9c50aea197839824f7899aeb103fb0265d0e
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-12-02 22:25:26 +0200

    Uodate task 10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -12,12 +12,25 @@
 //     .catch(error => {
 //         console.log(error);
 //     });
-import axios from 'axios';
 
-export function fetchCountries(name) {
-  return axios
-    .get(
-      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-    )
-    .then(({ data }) => data);
-}
+
+// import axios from 'axios';
+
+// export function fetchCountries(name) {
+//   return axios
+//     .get(
+//       `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+//     )
+//     .then(({ data }) => data);
+// }
+
+
+
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+.then(response => {
+  return response.json()
+})}
+
+
+export default { fetchCountries };
